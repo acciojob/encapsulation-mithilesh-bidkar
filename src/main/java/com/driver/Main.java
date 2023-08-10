125b68babc96bfe13406257447be13415a0064e4
mithilesh-bidkar <mithilesh.bidkar@gmail.com>
2023-08-10 18:27:35 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+//        r.name = "sai";   we cannot access name variable directly as it is private
+
+        r.setName("Acciojob");
+        System.out.println(r.getName());
+    }
 }
